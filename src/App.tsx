94124c22619fcc920b26bfc1d0900760156e2a34
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-02-01 08:58:46 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,35 +16,31 @@ import SubmitCase from "./pages/SubmitCase";
 import Appointments from "./pages/Appointments";
 import LegalResources from "./pages/LegalResources";
 import Settings from "./pages/Settings";
-import MyCases from "./pages/MyCases";
-import { LanguageProvider } from "./i18n";
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <LanguageProvider>
-        <TooltipProvider>
-          <Toaster />
-          <Sonner />
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
 
-          <Routes>
-            <Route path="/my-cases" element={<MyCases />} />
-            <Route path="/" element={<Index />} />
-            <Route path="/appointments" element={<Appointments />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/dashboard" element={<CitizenDashboard />} />
-            <Route path="/lawyer-dashboard" element={<LawyerDashboard />} />
-            <Route path="/judge-dashboard" element={<JudgeDashboard />} />
-            <Route path="/clerk-dashboard" element={<CourtClerkDashboard />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="/legal-resources" element={<LegalResources />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/find-lawyer" element={<FindLawyer />} />
-            <Route path="/submit-case" element={<SubmitCase />} />
-          </Routes>
-        </TooltipProvider>
-      </LanguageProvider>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/appointments" element={<Appointments />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/dashboard" element={<CitizenDashboard />} />
+          <Route path="/lawyer-dashboard" element={<LawyerDashboard />} />
+          <Route path="/judge-dashboard" element={<JudgeDashboard />} />
+          <Route path="/clerk-dashboard" element={<CourtClerkDashboard />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/legal-resources" element={<LegalResources />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/find-lawyer" element={<FindLawyer />} />
+          <Route path="/submit-case" element={<SubmitCase />} />
+        </Routes>
+
+      </TooltipProvider>
     </QueryClientProvider>
   );
 };
